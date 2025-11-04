097493990eb18a3e14a0a8cd7e1ada4227fe93b3
Mikhail <m3-kolom@yandex.ru>
2025-11-03 22:46:04 -0800

    Initial Commit

---
+++ BuildChecklists/BuildChecklistsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildChecklistsApp.swift
+//  BuildChecklists
+//
+//  Created by Михаил Коломиец on 03.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct BuildChecklistsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
